fa6e38b13b556399fa603cf1083abdde7a58352f
Nandini Bhagat <bhagatnandiniasn@gmail.com>
2026-08-05 19:22:25 +0530

    Connected to Database

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
-
 const app = require("./src/app");
+const connectDB = require("./src/db/db");
 
-const PORT=process.env.PORT
+connectDB();
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
