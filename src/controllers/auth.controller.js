e12cb45457762744de0d63b20cb914823dabbc7a
Nandini Bhagat <bhagatnandiniasn@gmail.com>
2026-08-05 21:48:03 +0530

    Implemented user login API with JWT authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -43,4 +43,49 @@ async function registerUser(req, res) {
   });
 }
 
-module.exports = { registerUser };
+async function loginUser(req, res) {
+  const { username, email, password } = req.body;
+
+  const user = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  // If user is invalid
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid Credentials",
+    });
+  }
+
+  // To check the validity of password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  // If password is not valid
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "Invalid Credentials",
+    });
+  }
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+  );
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User logged in successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+  });
+}
+
+module.exports = { registerUser, loginUser };
