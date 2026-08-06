a97850e303720972b04360a191bbc66d548f5799
Nandini Bhagat <bhagatnandiniasn@gmail.com>
2026-08-06 19:09:14 +0530

    Added APIs to fetch music and album data

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -46,4 +46,43 @@ async function createAlbum(req, res) {
   });
 }
 
-module.exports = { createMusic, createAlbum };
+async function getAllMusics(req, res) {
+  const musics = await musicModel.find().limit(10);
+
+  res.status(200).json({
+    message: "Musics fetched successfully",
+    musics: musics,
+  });
+}
+
+async function getAllAlbums(req, res) {
+  const albums = await albumModel
+    .find()
+    .select("title artist")
+    .populate("artist", "username email");
+  res.status(200).json({
+    message: "Albums fetched successfully",
+    albums: albums,
+  });
+}
+
+async function getAlbumById(req, res) {
+  const albumId = req.params.albumId;
+  const album = await albumModel
+    .findById(albumId)
+    .populate("artist", "username email")
+    .populate("musics");
+
+  return res.status(200).json({
+    message: "Album fetched successfully",
+    album: album,
+  });
+}
+
+module.exports = {
+  createMusic,
+  createAlbum,
+  getAllMusics,
+  getAllAlbums,
+  getAlbumById,
+};
--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -8,7 +8,23 @@ const upload = multer({
 });
 
 const router = express.Router();
-router.post("/upload", authMiddleware.authArtist, upload.single("music"), musicController.createMusic);
+router.post(
+  "/upload",
+  authMiddleware.authArtist,
+  upload.single("music"),
+  musicController.createMusic,
+);
+
 router.post("/album", authMiddleware.authArtist, musicController.createAlbum);
+
+router.get("/", authMiddleware.authUser, musicController.getAllMusics);
+
+router.get("/albums", authMiddleware.authUser, musicController.getAllAlbums);
+
+router.get(
+  "/albums/:albumId",
+  authMiddleware.authUser,
+  musicController.getAlbumById,
+);
 
 module.exports = router;
